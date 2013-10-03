23850f10ceca95b4646a2270648c4ed3ac69dac4
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-10-03 14:26:04 -0400

    Fight Loop

--- Week1/Rodriguez_Julian_TheDuel1/js/script.js
+++ Week1/Rodriguez_Julian_TheDuel1/js/script.js
@@ -14,14 +14,28 @@ Goal 1 Duel 1 Assignment
     var playerTwoName = "Batman";
 
     //Player Damage
-    var playerOneDMG =10;
-    var playerTwoDMG =10;
+    var playerOneDMG =50;
+    var playerTwoDMG =50;
 
     //Player Health
     var playerOneHealth = 100;
-    var PlayerTwoHealth = 100;
+    var playerTwoHealth = 100;
 
-    function fight(){};
+    function fight(){
+        alert (playerOneName+":"+playerOneHealth+"START"+playerTwoName+":"+playerTwoHealth);
+        for (var i=0; i<10;i++){
+
+        var minDamage1 = playerOneDMG* .5
+        var minDamage2 = playerTwoDMG* .5
+        var p1=Math.floor(Math.random()*(playerOneDMG-minDamage1)+minDamage1);
+        var p2=Math.floor(Math.random()*(playerTwoDMG-minDamage2)+minDamage2);
+
+
+        };
+
+    };
 
     function winnerCheck(){};
+
+    fight();
 })();
