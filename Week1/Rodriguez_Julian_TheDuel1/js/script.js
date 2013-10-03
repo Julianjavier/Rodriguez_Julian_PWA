9d6aa6a9bf08e8b7560f36b3e257dea9538512ab
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-10-03 15:37:57 -0400

    run fight correction

--- Week1/Rodriguez_Julian_TheDuel1/js/script.js
+++ Week1/Rodriguez_Julian_TheDuel1/js/script.js
@@ -7,7 +7,7 @@ Goal 1 Duel 1 Assignment
 //self executing function
 
 (function(){
-    console.log("LETS ROCK")
+    console.log("LETS ROCK");
 
     //Player Names
     var playerOneName = "Spiderman";
@@ -21,25 +21,38 @@ Goal 1 Duel 1 Assignment
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
+    var round=0;
+
     function fight(){
-        alert (playerOneName+":"+playerOneHealth+"START"+playerTwoName+":"+playerTwoHealth);
+        alert (playerOneName+":"+playerOneHealth+"START"+playerTwoName+":"+playerTwoHealth)
         for (var i=0; i<10;i++){
 
         var minDamage1 = playerOneDMG* .5
         var minDamage2 = playerTwoDMG* .5
-        var p1=Math.floor(Math.random()*(playerOneDMG-minDamage1)+minDamage1);
-        var p2=Math.floor(Math.random()*(playerTwoDMG-minDamage2)+minDamage2);
+        var p1=Math.floor(Math.random()*(playerOneDMG-minDamage1)+minDamage1)
+        var p2=Math.floor(Math.random()*(playerTwoDMG-minDamage2)+minDamage2)
 
          //inflict damage
          playerOneHealth-=p1;
          playerTwoHealth-=p2;
-          console.log(playerOneName+":"+playerOneHealth+"START"+playerTwoName+":"+playerTwoHealth);
+         console.log(playerOneName+":"+playerOneHealth+"START"+playerTwoName+":"+playerTwoHealth)
 
             var results= winnerCheck();
 
-        };
+            if(results === "NO WINNER"){
+                round++;
+                alert(playerOneName+":"+playerOneHealth+"ROUND"+round+"Over"+playerTwoName+":"+playerTwoHealth)
+            } else{
+                alert(results)
+                break;
 
-    };
+            }
+
+        }
+
+    }
+
+    fight();
 
     function winnerCheck(){
 
@@ -53,6 +66,5 @@ Goal 1 Duel 1 Assignment
 
         return result;
     };
-
 
 })();
