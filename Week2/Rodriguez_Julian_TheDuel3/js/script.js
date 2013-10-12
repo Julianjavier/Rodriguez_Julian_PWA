ccb9642d580d5262fab21472a5475d204c5f0b18
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-10-12 14:51:20 -0400

    Dom alocation

--- Week2/Rodriguez_Julian_TheDuel3/js/script.js
+++ Week2/Rodriguez_Julian_TheDuel3/js/script.js
@@ -9,7 +9,10 @@ Goal 1 Duel 1 Assignment
 (function(){
     console.log("LETS ROCK");
 
-  var playerData = [player1,player2]
+  var button= document.querySelector(".buttonblue")
+
+    button.addEventListener("click",fight)
+
 
   var player1 ={Name:"Spiderman",
                 Damage:20,
@@ -20,53 +23,63 @@ Goal 1 Duel 1 Assignment
                 Damage:20,
                 Health:100}
 
+  var playerData = [player1,player2]
 
 
-
-    var round=0;
+    var round=1;
 
     //calculates player health and damage
     function fight(){
         //alert (playerData1[0]+":"+playerData1[2]+" START "+playerData2[0]+":"+playerData2[2])
-        for (var i=0; i<10;i++){
+         var roundUpdate= document.querySelector("input")
+         var spiderHealth = document.querySelector("#kabal")
+         var batHealth = document.querySelector("#kratos")
 
-        var minDamage1 = playerData1[1]* .5
-        var minDamage2 = playerData2[1]* .5
-        var p1=Math.floor(Math.random()*(playerData1[1]-minDamage1)+minDamage1)
-        var p2=Math.floor(Math.random()*(playerData2[1]-minDamage2)+minDamage2)
+         roundUpdate.value= "Round "+round
+
+        var minDamage1 = playerData[0].Damage* .5
+        var minDamage2 = playerData[1].Damage* .5
+        var p1=Math.floor(Math.random()*(playerData[0].Damage-minDamage1)+minDamage1)
+        var p2=Math.floor(Math.random()*(playerData[1].Damage-minDamage2)+minDamage2)
 
          //inflict damage
-            playerData1[2]-=p1;
-            playerData2[2]-=p2;
-         console.log(playerData1[0]+":"+playerData1[2]+" START "+playerData2[0]+":"+playerData2[2])
+            playerData[0].Health-=p1;
+            playerData[1].Health-=p2;
+         console.log(playerData[0].Name+":"+playerData[0].Health+" START "+playerData[1].Name+":"+playerData[1].Health)
 
             var results= winnerCheck();
 
             if(results === "NO WINNER"){
                 round++;
+                spiderHealth.innerHTML= playerData[0].Health
+                batHealth.innerHTML= playerData[1].Health
+
                 //alert(playerData1[0]+":"+playerData1[2]+" ROUND"+round+" Over "+playerData2[0]+" : "+playerData2[2])
             } else{
-                //alert(results)
-                break;
+                roundUpdate.value= results;
 
             }
-
         }
 
-    }
 
-    fight();
     //verifies the winner after values are calculated.
     function winnerCheck(){
+
+        var button= document.querySelector(".buttonblue")
+
           var result= "NO WINNER"
 
-        if (playerData1[2]<1 && playerData2[2]<1){
-            result="DRAW"
-        } else if (playerData1[2]<1){
-            result=playerData1[0]+" WINS"
-        } else if (playerData1[2]<1){
-            result=playerData1[0]+" WINS"
+        if (playerData[0].Health<1 && playerData[1].Health<1){
+            result="DRAW";
+            button.removeEventListener("click", fight);
+            }else if (playerData[0].Health<1){
+            result=playerData[1].Name+" WINS";
+            button.removeEventListener("click", fight);
+        } else if (playerData[1].Health<1 ){
+            result=playerData[0].Name+" WINS";
+            button.removeEventListener("click", fight);
         };
+
 
         return result;
     };
